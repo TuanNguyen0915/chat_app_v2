ce6d425bb9429503583bc62a56fd8576c81f4e92
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-07 22:51:41 -0500

    add timeStamp for user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,29 +1,32 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ['male', 'female'],
+    },
+    profilePhoto: {
+      type: String,
+      default: 'https://www.pngmart.com/files/21/Account-Avatar-Profile-PNG-Photo.png',
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ['male', 'female'],
-  },
-  profilePhoto: {
-    type: String,
-    default: 'https://www.pngmart.com/files/21/Account-Avatar-Profile-PNG-Photo.png',
-  },
-})
+  { timestamps: true }
+)
 
 const User = mongoose.model('User', userSchema)
 export default User
